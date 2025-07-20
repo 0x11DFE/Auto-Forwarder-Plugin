417064b2818d37f7f8624257d25a6f36cfee50c0
Tesla <43712252+0x11DFE@users.noreply.github.com>
2025-07-20 19:22:08 +0200

    chore: Bump version to 1.6.5

--- auto_forwarder.py
+++ auto_forwarder.py
@@ -82,7 +82,7 @@ __id__ = "auto_forwarder"
 __name__ = "Auto Forwarder"
 __description__ = "Sets up forwarding rules for any chat, including users, groups, and channels."
 __author__ = "@T3SL4"
-__version__ = "1.6.4"
+__version__ = "1.6.5"
 __min_version__ = "11.9.1"
 __icon__ = "Putin_1337/14"
 
